15f6461b6ad2cad0e57adf0c464be768ccc34810
Aleksandr Mingalev <amingalev@gmail.com>
2023-09-09 18:25:57 +0200

    feat: complete almost all Numbers task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -183,8 +183,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-  return num.toFixed(3);
+  const n = num / (10 ** pow);
+  return Math.round(n) * 10 ** pow;
 }
+
 
 /**
  * Returns true is the number is prime; otherwise false.
@@ -203,8 +205,19 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) {
+    return true;
+  }
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -222,8 +235,10 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (value === null) { return def; }
+  if (Number.isInteger(Number(value)) === true) { return value; }
+  return def;
 }
 
 module.exports = {
